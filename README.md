c400db2ca0486fa6e2d2a30c037eefc0082cd718
Leonardo Garcia <leogsantos2017@gmail.com>
2023-07-03 20:17:57 -0300

    better output

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # PlaylistScraper
 
-A script for scraping video titles and links from a given Youtube playlist page. It uses Selenium to dynamically load the page and retrieve every element. The script outputs a CSV file containing three columns: data (including title, channel, video age, and views), title, and link.
+A script for scraping video titles and links from a given Youtube playlist page. It uses Selenium to dynamically load the page and retrieve every element. The script outputs a CSV file containing two columns: title, and link.
 Because it has to load every element, it's a little slow :(
+
+# TODO
+Fill output with other elements, like view count and age.
--- playlistscraper.py
+++ playlistscraper.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By 
+from sys import argv
 
+url = ""
+
+try:
+    url = argv[1]
+except IndexError:
+    url = input("type your link: ")
+
+#Setting up driver and fully loading the webpage
 opts = Options()
 opts.add_argument('--headless')
 driver = webdriver.Chrome('chromedriver', options=opts)
-url = 'https://www.youtube.com/playlist?list=PLP4CSgl7K7or84AAhr7zlLNpghEnKWu2c'
 driver.maximize_window()
 driver.get(url)
 while True:
@@ -17,16 +25,21 @@ while True:
     document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
     if html_before == driver.page_source:
         break
-div_elements = driver.find_elements(By.TAG_NAME, "ytd-playlist-video-renderer")
 
+#Playlist title for output csv
+playlist_title = driver.title
+
+#Get every element that points to a video
+div_elements = driver.find_elements(By.TAG_NAME, "ytd-playlist-video-renderer")
 videos = []
 for div in div_elements:
     video = div.find_element(By.TAG_NAME, 'h3')
     title = video.find_element(By.TAG_NAME, 'a')
-    videos.append([video.get_attribute("aria-label"), title.get_attribute('title'), title.get_attribute('href')])
+    videos.append([title.get_attribute('title'), title.get_attribute('href')])
 
-f = open('playlists.csv', 'w', encoding='utf-8')
-f.write("data, title, link\n")
+#Output to csv
+f = open((playlist_title + '.csv'), 'w', encoding='utf-8')
+f.write("title, link\n")
 for video in videos:
-    f.write("\"" + video[0]+ "\",\"" + video[1] + "\"," + video[2] + "\n")
+    f.write("\"" + video[0] + "\"," + video[1] + "\n")
 driver.close()
